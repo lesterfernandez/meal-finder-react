b0976d1aee14b82b477257b59af1b626deec5bde
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-10-06 18:29:04 -0400

    first commit

--- src/components/styled/GlobalStyles.styled.js
+++ src/components/styled/GlobalStyles.styled.js
@@ -2,7 +2,16 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
 
-  
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #2d2013;
+    color: white;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    margin: 0;
+}
 
 `;
 
